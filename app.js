d14563cc071230bb591b76037dfc409806f6e340
Cookie <lucassrossi12@gmail.com>
2025-12-31 19:28:14 -0300

    add

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const indexRoute = require("./routes/index.js");
 const messageRouter = require("./routes/message.js");
 const newMessageRouter = require("./routes/newMessage.js");
+const path = require("node:path")
 const express = require("express")
 
 const app = express()
@@ -11,6 +12,9 @@ app.set("view engine", "ejs")
 app.use("/{index}", indexRoute)
 app.use("/message{s}", messageRouter)
 app.use("/new-message{s}", newMessageRouter)
+app.use((req, res) => {
+  res.status(404).render('404')
+})
 
 const PORT = 8080;
 
